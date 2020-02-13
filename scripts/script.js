0aaccf6bd03cccff11ce182999c4b23eb6ce394f
Jobert Manosca <joberttm@hotmail.com>
2020-02-13 00:10:15 -0500

    styled the app section

--- scripts/script.js
+++ scripts/script.js
@@ -449,12 +449,17 @@ colorBook.init = function() {
   $("input[name='page']").click(function() {
     const selectedPage = parseInt($(this).val());
     if(currentPage !== selectedPage){
+      // Check if the previous page is finished
       if(selectedPage === 0 || colorBook.progress[selectedPage - 1].isCompleted){
         colorBook.canvas = colorBook.progress[selectedPage].page;
         colorBook.redrawCanvas();
         currentPage = selectedPage;
       } else {
         const alertString = `You must complete Page ${selectedPage} before you can view this page.`;
+
+        // Makes sure that the selected tab doesn't get checked anyway
+        $(`input[name='page'][value=${currentPage}]`).prop("checked", true);
+        
         alert(alertString);
       }
     }
